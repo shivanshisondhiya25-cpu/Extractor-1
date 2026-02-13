37b85827106187891ece72c6b60b0f570897f5e1
shivanshisondhiya25-cpu <shivanshisondhiya25@gmail.com>
2026-02-13 14:55:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7630552476:AAGdSH7FUl3IqwzTbLQIuaRC3eMv46Qre-A")
+    API_ID = int(os.environ["API_ID",
+"25513009"]
+    API_HASH = os.environ["API_HASH",
+"33ace9b0779df4387a8ad8b7fbe6b050"]
+    AUTH_USERS = ""7068035738""
+ 
